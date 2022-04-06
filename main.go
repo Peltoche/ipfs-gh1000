ee2cde97c44302a47aff570530063d7e1b1a9d22
Pierre Peltier <pierre.peltier-ext@adevinta.com>
2022-04-06 16:06:05 +0200

    Do a basic fetch of gitstar-ranking.com

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	res, err := http.Get("https://gitstar-ranking.com/repositories")
+	if err != nil {
+		log.Fatalf("failed to get the gitstar-ranking.com page: %s", err)
+	}
+	defer func() { _ = res.Body.Close() }()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("failed to read the body of gitstar-ranking.com: %s", err)
+	}
+
+	fmt.Printf("body: %q", body)
 }
